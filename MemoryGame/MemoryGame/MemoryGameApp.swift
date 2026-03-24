2608f6aca0cb6bfc49a3c3bdd3ffed4ca33f5fce
nishan2007 <nishannarain@gmail.com>
2026-03-24 15:15:37 -0400

    Memory Game

---
+++ MemoryGame/MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Nishan Narain on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
